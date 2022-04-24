f0c698ceddaaa0186fea5a38ad3bcd4533320d65
2rjo <arno.ahmed2546@gmail.com>
2022-04-24 14:52:13 +0600

    :tada: init services

---
+++ ZebraPrinter.WPF/Services/CommonHttpService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace posdesktop.Services
+{
+    class CommonHttpService<T>
+    {
+        public T Get(string Url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConnectionString.GetPosTestApiIP);
+                var responseTask = client.GetAsync(Url);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<T>();
+                    readTask.Wait();
+                    var T = readTask.Result;
+                    return T;
+                }
+                else
+                {
+                    return default;
+                }
+            }
+        }
+        public IEnumerable<T> GetAll(string Url, string uriString = null)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uriString == null ? new Uri(ConnectionString.GetPosTestApiIP) : new Uri(uriString);
+                var responseTask = client.GetAsync(Url);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<T[]>();
+                    readTask.Wait();
+                    var Ts = readTask.Result;
+                    return Ts;
+                }
+                else
+                {
+                    return new List<T>();
+                }
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetAllAsync(string Url)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConnectionString.GetPosTestApiIP);
+                var responseTask = await client.GetAsync(Url);
+                return responseTask;
+            }         
+        }
+
+        public int put(string url, T entity)
+        {
+            using (var client = new HttpClient()){                
+                client.BaseAddress = new Uri(ConnectionString.GetPosTestApiIP);
+                var response = client.PutAsJsonAsync(url, entity).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return 1;
+                }
+                else
+                    return -1;
+            }
+        }
+
+        public List<T> PostBatch(string url, List<T> entity)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConnectionString.GetPosTestApiIP);
+                var responseTask = client.PostAsJsonAsync(url, entity);
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<T[]>();
+                    readTask.Wait();
+                    var Ts = readTask.Result;
+                    return Ts.ToList();
+                }
+                else
+                {
+                    return new List<T>();
+                }
+            }
+        }
+
+        public HttpResponseMessage Post(string url, T entity)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConnectionString.GetPosTestApiIP);
+                var responseTask = client.PostAsJsonAsync(url, entity);
+                responseTask.Wait();
+                var result = responseTask.Result;
+                return result;
+                //if (result.IsSuccessStatusCode)
+                //{
+                //    //var readTask = result.Content.ReadAsAsync<T[]>();
+                //    //readTask.Wait();
+                //    //var Ts = readTask.Result;
+                //    //return Ts.ToList();
+                //}
+                //else
+                //{
+                //    //return new List<T>();
+                //}
+            }
+        }
+    }
+}
---
+++ ZebraPrinter.WPF/Services/ConnectionString.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+
+namespace posdesktop.Services
+{
+    public static class ConnectionString
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+        private static string PosTestApiIP = ConfigurationManager.AppSettings["PosTestApiIP"];
+        private static string TwelveApiIP = ConfigurationManager.AppSettings["TwelveApiIP"];
+        public static string GetPosApiTestCS
+        {
+            get
+            {
+                return connectionString;
+            }
+            set
+            {
+                connectionString = value;
+            }
+        }
+        public static string GetTwelveApiIP { get { return TwelveApiIP; } }
+        public static string GetPosTestApiIP
+        {
+
+            get
+            {
+                return PosTestApiIP;
+            }
+            set
+            {
+                PosTestApiIP = value;
+            }
+        }
+    }
+    public static class CSTWelveERP
+    {
+        private static string connectionString = ConfigurationManager.AppSettings["PosTestApi"];
+
+        public static string GetConnectionString
+        {
+            get
+            {
+                return connectionString;
+            }
+            set
+            {
+                connectionString = value;
+            }
+        }
+    }
+
+}
